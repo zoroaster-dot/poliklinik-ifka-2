02f915c0832526727ad0b0db32814e73cc182c6c
Ifka Nabilah <85624063+zoroaster-dot@users.noreply.github.com>
2023-12-31 19:34:01 +0700

    first commit

--- database/seeders/DokterSeeder.php
+++ database/seeders/DokterSeeder.php
@@ -16,22 +16,28 @@ class DokterSeeder extends Seeder
     {
         DB::table('dokters')->insert([
             [
-                'nama' => 'Dr. Andi',
-                'alamat' => 'Jl. Raya No. 2',
-                'no_hp' => '081234567890',
+                'nama' => 'dr. Tri',
+                'alamat' => 'Jl. Indraprasta No. 72',
+                'no_hp' => '082156897852',
                 'id_poli' => 1,
             ],
             [
-                'nama' => 'Dr. Budi',
-                'alamat' => 'Jl. Raya No. 3',
-                'no_hp' => '081234567891',
+                'nama' => 'dr. Ghinaa',
+                'alamat' => 'Jl. Sriwijaya No. 13',
+                'no_hp' => '082365897852',
                 'id_poli' => 2,
             ],
             [
-                'nama' => 'Dr. Caca',
-                'alamat' => 'Jl. Raya No. 4',
-                'no_hp' => '081234567892',
+                'nama' => 'dr. Zahra',
+                'alamat' => 'Jl. Imam Bonjol No. 98',
+                'no_hp' => '082356897854',
                 'id_poli' => 3,
+            ],
+            [
+                'nama' => 'dr. Wahyu',
+                'alamat' => 'Jl. Kelud No. 122',
+                'no_hp' => '081234567890',
+                'id_poli' => 4,
             ],
 
         ]);
