fca5bd75171cae600ceb652ee6726460a0b117a2
Ifka Nabilah <85624063+zoroaster-dot@users.noreply.github.com>
2023-12-31 19:35:46 +0700

    first commit

--- database/seeders/JadwalSeeder.php
+++ database/seeders/JadwalSeeder.php
@@ -18,26 +18,32 @@ class JadwalSeeder extends Seeder
             [
                 'hari' => 'Senin',
                 'jam_mulai' => '08:00',
-                'jam_selesai' => '12:00',
+                'jam_selesai' => '11:30',
                 'id_dokter' => 1,
             ],
             [
                 'hari' => 'Selasa',
                 'jam_mulai' => '08:00',
-                'jam_selesai' => '12:00',
+                'jam_selesai' => '10:00',
                 'id_dokter' => 2,
             ],
             [
                 'hari' => 'Rabu',
-                'jam_mulai' => '08:00',
-                'jam_selesai' => '12:00',
+                'jam_mulai' => '15:30',
+                'jam_selesai' => '18:00',
                 'id_dokter' => 3,
             ],
             [
                 'hari' => 'Kamis',
-                'jam_mulai' => '08:00',
-                'jam_selesai' => '12:00',
+                'jam_mulai' => '17:00',
+                'jam_selesai' => '19:30',
                 'id_dokter' => 1,
+            ],
+            [
+                'hari' => 'Rabu',
+                'jam_mulai' => '18:00',
+                'jam_selesai' => '20:00',
+                'id_dokter' => 4,
             ],
 
         ]);
